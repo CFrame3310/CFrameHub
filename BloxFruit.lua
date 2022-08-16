e0e3794a30fd9dd32857c64a48a6c19d7ff4757d
CFrame3310 <111138183+CFrame3310@users.noreply.github.com>
2022-08-16 20:05:27 +0700

    Update BloxFruit.lua

--- BloxFruit.lua
+++ BloxFruit.lua
@@ -705,7 +705,8 @@ spawn(function()
                     end
                 else
                     Tween(CFrame.new(0,10,0) * CFrameQuest)
-                    if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position -  CFrameQuest.Position).Magnitude <= 15 then
+                    if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position -  CFrameQuest.Position).Magnitude <= 10 then
+                        wait(1)
                         game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("StartQuest",NQuest,LQuest)
                     end
                 end
