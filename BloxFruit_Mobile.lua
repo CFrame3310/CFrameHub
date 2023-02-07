2d8a879b57ecd1dc4d833f43eb9469d8167788c2
CFrame3310 <111138183+CFrame3310@users.noreply.github.com>
2023-02-07 08:31:52 +0700

    Update BloxFruit_Mobile.lua

--- BloxFruit_Mobile.lua
+++ BloxFruit_Mobile.lua
@@ -1,3 +1,3 @@
-game.Players.LocalPlayer:Kick('Please join Discrod to get new version \n discord.gg/VERTxGCaFp')
+game.Players.LocalPlayer:Kick('This script is discontinued, Please join Discrod to get new script discord.gg/VERTxGCaFp')
 wait(10)
 game:Shutdown()
