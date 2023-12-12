f9c0276742f8058c96993226430e2735400a03f2
CFrame <111138183+CFrame3310@users.noreply.github.com>
2023-12-12 17:50:04 +0700

    Update LICENSE_ID.lua

--- LICENSE_ID.lua
+++ LICENSE_ID.lua
@@ -1,6 +1,7 @@
 return {
   ['Blox Fruit'] = {
     ['License'] = 'CCXLC-P0CW5-NGATN-S9Z7X-7P103',
+    ['Free Use'] = true,
     ['Message'] = 'Invaild License ID, join discord server to fix.\ndiscord.gg/VERTxGCaFp'
   },
   ['Bee Swarm Siumlator'] = nil
