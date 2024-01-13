927fd98ebe84e1936964b82da4f9569894f39dce
CFrame <111138183+CFrame3310@users.noreply.github.com>
2024-01-13 17:13:39 +0700

    Update Configuration.lua

--- Configuration.lua
+++ Configuration.lua
@@ -1,11 +1,13 @@
 return {
   ['Blox Fruit'] = {
-    ['BF Version'] = 'CCXLC-P0CW5-NGATN-S9Z7X-7P103',
-    ['Free Use'] = true,
-    ['Message'] = 'Invaild License ID, join discord server to fix.\ndiscord.gg/VERTxGCaFp'
+    ['Version'] = 'CCXLC-P0CW5-NGATN-S9Z7X-7P103',
+    ['Premium'] = false,
+    ['Config] = {
+      ['Error'] = 'Invaild Version Please try again later or join.\ndiscord.gg/VERTxGCaFp'
+    }
   },
   ['Bee Swarm Siumlator'] = {
-    ['BSS Version'] = '1.0',
+    ['Version'] = '1.0',
     ['Premium'] = false,
     ['Config] = {
       ['Error'] = 'Invaild Version Please try again later or join.\ndiscord.gg/VERTxGCaFp'
