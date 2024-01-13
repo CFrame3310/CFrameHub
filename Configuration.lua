b6c3bf6a13e45845860f194cc7e8e29ffe7fab05
CFrame <111138183+CFrame3310@users.noreply.github.com>
2024-01-13 17:10:39 +0700

    Update and rename LICENSE_ID.lua to Configuration.lua

---
+++ Configuration.lua
@@ -0,0 +1,14 @@
+return {
+  ['Blox Fruit'] = {
+    ['BF Version'] = 'CCXLC-P0CW5-NGATN-S9Z7X-7P103',
+    ['Free Use'] = true,
+    ['Message'] = 'Invaild License ID, join discord server to fix.\ndiscord.gg/VERTxGCaFp'
+  },
+  ['Bee Swarm Siumlator'] = {
+    ['BSS Version'] = '1.0',
+    ['Premium'] = false,
+    ['Config] = {
+      ['Error'] = 'Invaild Version Please try again later or join.\ndiscord.gg/VERTxGCaFp'
+    }
+  }
+}
--- LICENSE_ID.lua
+++
@@ -1,8 +0,0 @@
-return {
-  ['Blox Fruit'] = {
-    ['License'] = 'CCXLC-P0CW5-NGATN-S9Z7X-7P103',
-    ['Free Use'] = true,
-    ['Message'] = 'Invaild License ID, join discord server to fix.\ndiscord.gg/VERTxGCaFp'
-  },
-  ['Bee Swarm Siumlator'] = nil
-}
