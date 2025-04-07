f2e362fa1290f06ec4d6fdc7401602c70d4bc99a
CFrame <111138183+CFrame3310@users.noreply.github.com>
2025-04-07 18:58:16 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,13 +5,15 @@ function loadScript(url)
 
   if response == '404: Not Found' then
     game.StarterGui:SetCore("SendNotification", {Title = 'CFrame Hub', Text = 'Updating Script, please try again in few minutes',Duration = 8})
+  else
+    loadstring(response)()
   end
 end
 
 local placeid = game.PlaceId
 
 if placeid == 87039211657390 or placeid == 4442272183 or placeid == 128336380114944 then 
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/Arise.lua"))()
+  loadScript(game:HttpGet("https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/Arise.lua"))()
 else
   game.Players.LocalPlayer:Kick('Script doesn\'t support this game.')
   wait(3)
