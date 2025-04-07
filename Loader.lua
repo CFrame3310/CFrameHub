46ef978fe68a96c173778d3892178274cf6f57c8
CFrame <111138183+CFrame3310@users.noreply.github.com>
2025-04-07 19:00:17 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -13,7 +13,7 @@ end
 local placeid = game.PlaceId
 
 if placeid == 87039211657390 or placeid == 4442272183 or placeid == 128336380114944 then 
-  loadScript(game:HttpGet("https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/Arise.lua"))()
+  loadScript("https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/Arise.lua")
 else
   game.Players.LocalPlayer:Kick('Script doesn\'t support this game.')
   wait(3)
